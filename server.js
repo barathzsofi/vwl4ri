d313d8e3b42fd8b154f5cb9b1a1e332c41704a8f
barathzsofi <barath3@freemail.hu>
2015-10-29 19:57:20 +0000

    make it better

--- server.js
+++ server.js
@@ -132,13 +132,20 @@ orm.initialize(waterlineConfig, function(err, models) {
     if(err) throw err;
     app.models = models.collections;
     app.connections = models.connections;
-    var port = process.env.PORT;
+    
+    /*var port = process.env.PORT;
     var host = process.env.IP;
     var server = app.listen(port, host, function(){
         //console.log('Server is started.');
         
-    });
+    });*/
     //console.log("ORM is started.");
+    
+    var port = process.env.PORT || 3000;
+    app.listen(port, function () {
+        console.log('Server is started.');
+    });
+    
 });
 
 
